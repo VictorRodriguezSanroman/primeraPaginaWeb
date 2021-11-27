6f5d322db4a120a6b180f20fc8ccc76e852928ba
Víctor Rodríguez Sanromán <victor.r.sanroman@gmail.com>
2021-11-27 18:11:07 +0100

    Añadidas sección de calendario y estadísticas

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+/* Scrpipt para crear imágenes aleatorias en la pag de fotos */
+
 let fotos = ['1.jpg','2.jpg','3.jpg','acuerdoFranciaItalia.jpg',
              'banderaHonduras.jpg','curry.jpg','FerAlonso.jpg',
              'FerAlonsoPodio.jpg','Portugal.jpeg','rayo.jpg','rayoAbrazo.jpg',
@@ -5,13 +7,12 @@ let fotos = ['1.jpg','2.jpg','3.jpg','acuerdoFranciaItalia.jpg',
 
 document.images["imagenes"].src = "img/" + fotos[0];
 console.log('numero de fotos: ' +fotos.length);
+
 let cambiaFotos = setInterval(muestraFotos,3000);
 
 function muestraFotos(){
     for (let i = 0; i < fotos.length; i++) {
         let aleatorio = Math.floor(Math.random() * fotos.length);
-        document.images["imagenes"].src = "img/" + fotos[aleatorio];
-
-        
+        document.images["imagenes"].src = "img/" + fotos[aleatorio];  
     }
 }
